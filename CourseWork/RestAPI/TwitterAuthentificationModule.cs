3cd923c859788be52f7283921b568cc1a355798d
Stelmashenko-A <stelma_28@bk.ru>
2015-10-09 13:08:48 +0300

    TwitterOauth

---
+++ CourseWork/RestAPI/TwitterAuthentificationModule.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using Nancy.Json;
+using Server;
+
+namespace RestAPI
+{
+    public class TwitterAuthentificationModule : NancyModule
+    {
+        public TwitterAuthentificationModule()
+        {
+            InitializeTweet();
+        }
+
+        protected void InitializeTweet()
+        {
+            Get["/twitter/authentification/authorizationUri"] = parameters =>
+            {
+                return TwitterOauth.GetAuthorizationUri();
+            };
+
+            Get["/twitter/authentification/pin"] = parameters =>
+            {
+                return new JavaScriptSerializer().Serialize("pin");
+            };
+        }
+    }
+}
---
+++ CourseWork/Server/TwitterOauth.cs
@@ -0,0 +1,17 @@
+﻿using TweetSharp;
+
+namespace Server
+{
+    public static class TwitterOauth
+    {
+        public static string GetAuthorizationUri()
+        {
+            var service = new TwitterService("не забыл стереть", "не забыл стереть 2.0");
+
+            var requestToken = service.GetRequestToken();
+
+            var uri = service.GetAuthorizationUri(requestToken);
+            return uri.ToString();
+        }
+    }
+}
