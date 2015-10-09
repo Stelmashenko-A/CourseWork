dc86809095f0da86eb0d26fe77112b82de960384
Stelmashenko-A <stelma_28@bk.ru>
2015-10-09 13:34:44 +0300

    AuthorizeCallback(string oauthToken, string oauthVerifier)

--- CourseWork/Server/TwitterOauth.cs
+++ CourseWork/Server/TwitterOauth.cs
@@ -6,12 +6,20 @@ namespace Server
     {
         public static string GetAuthorizationUri()
         {
-            var service = new TwitterService("не забыл стереть", "не забыл стереть 2.0");
-
+            var service = new TwitterService("",
+                "");
             var requestToken = service.GetRequestToken();
-
             var uri = service.GetAuthorizationUri(requestToken);
             return uri.ToString();
+        }
+
+        public static string AuthorizeCallback(string oauthToken, string oauthVerifier)
+        {
+            var requestToken = new OAuthRequestToken {Token = oauthToken};
+            var service = new TwitterService("",
+                "");
+            var accessToken = service.GetAccessToken(requestToken, oauthVerifier);
+            return accessToken.Token;
         }
     }
 }
