c5746bf1e2cfa11ce10d2a7e5a0c42d546a7e7b2
Stelmashenko-A <stelma_28@bk.ru>
2015-11-09 15:47:31 +0300

    TestUtiles

--- CourseWork/Repository.Test/AccountRepositoryTests.cs
+++ CourseWork/Repository.Test/AccountRepositoryTests.cs
@@ -1,14 +1,14 @@
 ﻿using System.Linq;
-using LinqToTwitter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Server;
+using TestUtiles;
 
 namespace Repository.Test
 {
     [TestClass]
     public class AccountRepositoryTests
     {
-        readonly AccountRepository _accountRepository=new AccountRepository();
+        private readonly AccountRepository _accountRepository = new AccountRepository();
 
         [TestMethod]
         public void TestGetAccountById()
@@ -30,6 +30,7 @@ namespace Repository.Test
             var t = _accountRepository.GetTwitterCredentials(2765688547);
             Assert.IsNotNull(t);
         }
+
         [TestMethod]
         public void TestSetUserStatuses()
         {
@@ -37,22 +38,14 @@ namespace Repository.Test
             var accountRepository = new AccountRepository();
             var credentials = accountRepository.GetTwitterCredentials(2765688547);
             var statuses = initializer.LoadStatuses(credentials);
-            
+
             accountRepository.DelUserStatuses(2765688547);
-            accountRepository.SetUserStatuses(2765688547,statuses.ToList());
+            accountRepository.SetUserStatuses(2765688547, statuses.ToList());
 
-            var contextBuilder = new TwitterContextBuilder();
-            var twitterContext = contextBuilder.Build(accountRepository.GetTwitterCredentials(2765688547));
-            var userResponse =
-                (from user in twitterContext.User
-                 where user.Type == UserType.Lookup &&
-                       user.UserIdList == "2765688547"
-                 select user).ToListAsync();
-            userResponse.Wait();
-            var statusesCount = userResponse.Result[0].StatusesCount;
-
-            Assert.AreEqual(_accountRepository.Get(2765688547).UserStatuses.Statuses.Count, statusesCount);
-           
+            var statusCounter = new StatusCounter();
+            Assert.AreEqual(statusCounter.Count(2765688547, accountRepository),
+                _accountRepository.Get(2765688547).UserStatuses.Statuses.Count
+                );
         }
     }
 }
---
+++ CourseWork/TestUtiles/StatusCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Server;
+
+namespace TestUtiles
+{
+    public class StatusCounter
+    {
+        public int Count(ulong userId)
+        {
+            var contextBuilder = new TwitterContextBuilder();
+            var twitterContext = contextBuilder.Build(accountRepository.GetTwitterCredentials(2765688547));
+            var userResponse =
+                (from user in twitterContext.User
+                 where user.Type == UserType.Lookup &&
+                       user.UserIdList == "2765688547"
+                 select user).ToListAsync();
+            userResponse.Wait();
+            var statusesCount = userResponse.Result[0].StatusesCount;
+        }
+    }
+}
