c6be0e8c758cfc68e464765248fe425319257436
Stelmashenko-A <stelma_28@bk.ru>
2016-01-30 20:06:04 +0300

    TwitterUser

---
+++ CourseWork/Repository/Model/TwetterUser.cs
@@ -0,0 +1,31 @@
+﻿namespace Repository.Model
+{
+    public class TwetterUser
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string IdStr { get; set; }
+        public TwitterEntities Entities { get; set; }
+        public bool ContributorsEnabled { get; set; }
+        public int FavouritesCount { get; set; }
+        public string Url { get; set; }
+        public string ProfileImageUrlHttps { get; set; }
+        public int UtcOffset { get; set; }
+        public long Id { get; set; }
+        public int ListedCount { get; set; }
+        public string Lang { get; set; }
+        public int FollowersCount { get; set; }
+        public bool Protected { get; set; }
+        //  public object notifications { get; set; }
+        public bool Verified { get; set; }
+        public bool GeoEnabled { get; set; }
+        public string TimeZone { get; set; }
+        public string Description { get; set; }
+        public bool DefaultProfileImage { get; set; }
+        public string ProfileBackgroundImageUrl { get; set; }
+        public int StatusesCount { get; set; }
+        public int FriendsCount { get; set; }
+        public bool ShowAllInlineMedia { get; set; }
+        public string ScreenName { get; set; }
+    }
+}
--- CourseWork/Repository/Model/TwitterStatus.cs
+++ CourseWork/Repository/Model/TwitterStatus.cs
@@ -11,19 +11,17 @@ namespace Repository.Model
         public DateTime CreatedAt { get; set; }
         public string IdStr { get; set; }
         public TwitterEntities TwitterEntities { get; set; }
-        public long InReplyToUserIdStr { get; set; }
+        public string InReplyToUserIdStr { get; set; }
         public List<long> Contributors { get; set; }
         public string Text { get; set; }
         public int RetweetCount { get; set; }
-        public long InReplyToStatusIdStr { get; set; }
+        public string InReplyToStatusIdStr { get; set; }
         public long Id { get; set; }
         public bool Retweeted { get; set; }
         public bool PossiblySensitive { get; set; }
-        public long InReplyToUserId { get; set; }
         //  public object place { get; set; }
         public long UserId { get; set; }
         public string InReplyToScreenName { get; set; }
         public string Source { get; set; }
-        public long InReplyToStatusId { get; set; }
     }
 }
